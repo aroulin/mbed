e677e1dc009ae22e3d55605a160d0d9808a12cd7
aroulin <andy.roulin@epfl.ch>
2014-12-04 12:35:53 +0100

    Basic AT-Command Test

--- libraries/tests/gsm/basic/main.c
+++ libraries/tests/gsm/basic/main.c
@@ -2,17 +2,46 @@
 #include "gpio_api.h"
 #include "wait_api.h"
 #include "us_ticker_api.h"
+#include "serial_api.h"
+#include "c027_api.h"
+
+typedef int bool;
 
 static gpio_t led;
 static ticker_event_t ticker;
 static uint32_t tick_ms = 0;
 
-void ISR_ticker_handler(uint32_t id) {
+static serial_t serial_gsm;
+
+void ISR_ticker_handler(uint32_t id)
+{
     us_ticker_insert_event(&ticker, us_ticker_read() + 1000, (uint32_t) &ticker);
     // Toggling led every second
     tick_ms = (tick_ms + 1) % 1000;
     if(tick_ms == 0) {
         gpio_write(&led, !gpio_read(&led));
+    }
+}
+
+void ISR_serial_gsm(uint32_t id, SerialIrq event)
+{
+    if (event == RxIrq) {
+        char c = serial_getc(&serial_gsm);
+        if(c == '\r') {
+            printf("GSM: <CR>\n");
+        } else if(c == '\n') {
+            printf("GSM: <LF>\n");
+        } else {
+            printf("GSM: %c\n", c);
+        }
+    }
+}
+
+void serial_puts(serial_t *obj, const char *str)
+{
+    while (*str != 0) {
+        serial_putc(obj, *str);
+        str++;
     }
 }
 
@@ -23,6 +52,19 @@ int main(void)
     us_ticker_init();
     us_ticker_set_handler(&ISR_ticker_handler);
     us_ticker_insert_event(&ticker, us_ticker_read() + 1000, (uint32_t) &ticker);
+
+    c027_init();
+    c027_mdm_powerOn(0);
+    serial_init(&serial_gsm, MDMTXD, MDMRXD);
+    serial_irq_handler(&serial_gsm, &ISR_serial_gsm, (uint32_t) &serial_gsm);
+    serial_irq_set(&serial_gsm, RxIrq, 1);
+
+    // Enter command mode
+    wait(1);
+    serial_puts(&serial_gsm, "+++");
+    wait(1);
+    // Disable DTE flow control
+    serial_puts(&serial_gsm, "AT&K0\r");
 
     while(1);
 }
