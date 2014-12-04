ccebcf83473e5ab79b1af94dfd7bb1574801dc35
aroulin <andy.roulin@epfl.ch>
2014-12-04 11:57:20 +0100

    Blinking with interrupts

--- libraries/tests/gsm/basic/main.c
+++ libraries/tests/gsm/basic/main.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include "gpio_api.h"
 #include "wait_api.h"
+#include "us_ticker_api.h"
 
 static gpio_t led;
+static ticker_event_t ticker;
+static uint32_t tick_ms = 0;
+
+void ISR_ticker_handler(uint32_t id) {
+    us_ticker_insert_event(&ticker, us_ticker_read() + 1000, (uint32_t) &ticker);
+    // Toggling led every second
+    tick_ms = (tick_ms + 1) % 1000;
+    if(tick_ms == 0) {
+        gpio_write(&led, !gpio_read(&led));
+    }
+}
 
 int main(void)
 {
     gpio_init_out_ex(&led, LED, 0);
-    while(1) {
-        gpio_write(&led, 1);
-        wait(1);
-        gpio_write(&led, 0);
-        wait(1);
-    }
+
+    us_ticker_init();
+    us_ticker_set_handler(&ISR_ticker_handler);
+    us_ticker_insert_event(&ticker, us_ticker_read() + 1000, (uint32_t) &ticker);
+
+    while(1);
 }
